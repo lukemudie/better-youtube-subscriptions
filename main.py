46c264e7fae854ba58cb43751e8fa6f2f3da51fc
lukemudie <50330874+lukemudie@users.noreply.github.com>
2021-03-13 22:37:55 +0000

    Added main.py with some sample code from the google API documentation. Added .gitignore to stop the client_secret.json file from being committed

---
+++ main.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.subscriptions.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/guides/code_samples#python
+
+import os
+
+import google_auth_oauthlib.flow
+from google.auth.transport.requests import Request
+import googleapiclient.discovery
+import googleapiclient.errors
+
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "client_secret.json"
+
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+        
+    flow.run_local_server(port=8080, prompt="consent")
+        
+    credentials = flow.credentials
+    
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    request = youtube.playlists().list(
+        part="snippet,contentDetails",
+        maxResults=25,
+        mine=True
+    )
+    response = request.execute()
+
+    print(response)
+
+if __name__ == "__main__":
+    main()
