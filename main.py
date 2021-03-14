48410647b9a2c0369200b0c011cf99b4adab7960
lukemudie <50330874+lukemudie@users.noreply.github.com>
2021-03-14 21:22:02 +0000

    Updated main.py to save credentials in a local token.pickle (not committed) so that the user doesn't have to log in each time the script is run. The script now looks through a channel's videos (currently hardcoded to only look at 10) and selects and opens one at random

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-# -*- coding: utf-8 -*-
-
-# Sample Python code for youtube.subscriptions.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/guides/code_samples#python
-
 import os
+import pickle
 
 import google_auth_oauthlib.flow
 from google.auth.transport.requests import Request
@@ -12,6 +7,9 @@ import googleapiclient.discovery
 import googleapiclient.errors
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+from random import randint
+import webbrowser
 
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
@@ -21,26 +19,83 @@ def main():
     api_service_name = "youtube"
     api_version = "v3"
     client_secrets_file = "client_secret.json"
+    
+    # load in credentials from a pickle file if it exists
+    credentials = None
+    
+    if os.path.exists("token.pickle"):
+        print("Loading credentials from file...")
+        with open("token.pickle", "rb") as token:
+            credentials = pickle.load(token)
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-        
-    flow.run_local_server(port=8080, prompt="consent")
-        
-    credentials = flow.credentials
+    # if there are no valid credentials saved then either refresh the token or log in
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            print("Refreshing access token...")
+            credentials.refresh(Request())
+        else:
+            print("Fetching new tokens...")
+            flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+                client_secrets_file,
+                scopes
+            )
+            
+            flow.run_local_server(port=8080, prompt="consent")
+            credentials = flow.credentials
+            
+            with open("token.pickle", "wb") as f:
+                print("Saving credentials for future use...")
+                pickle.dump(credentials, f)
     
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials)
 
-    request = youtube.playlists().list(
-        part="snippet,contentDetails",
-        maxResults=25,
-        mine=True
+    # getting the channel information; we need the id of the uploads playlist
+    channel_request = youtube.channels().list(
+        part="contentDetails,statistics",
+        id="UCHZqZf6nbTu3hnRtOJwUtkA"
     )
-    response = request.execute()
+    channel_response = channel_request.execute()
 
-    print(response)
+    channel_id = channel_response['items'][0]['id']
+    
+    # need to generate a random number between 1 and videoCount, then we know we only need to page up to that point
+    max_vids = int(channel_response['items'][0]['statistics']['videoCount'])
+    ### SETTING MAX NUMBER TEMPORARILY TO 10 FOR TESTING ###
+    max_vids = 10
+    
+    if max_vids >= 50:
+        num_vids_to_pull = 50
+    else:
+        num_vids_to_pull = max_vids
+    
+    rand_vid_num = randint(1, max_vids)
+    
+    print("channel_id: " + channel_id)
+    print("number of videos: " + str(max_vids))
+    print("chosen video number: " + str(rand_vid_num))
+    
+    # getting the uploads playlist information so that we can get the url of the chosen random video
+    playlist_request = youtube.playlistItems().list(
+        part="contentDetails",
+        playlistId="UUHZqZf6nbTu3hnRtOJwUtkA",
+        maxResults=num_vids_to_pull
+    )
+    
+    playlist_response = playlist_request.execute()
+    
+    i = 1
+    for item in playlist_response['items']:
+        print("https://www.youtube.com/watch?v=" + item['contentDetails']['videoId'])
+        if i == rand_vid_num:
+            print(" - the chosen one")
+            webbrowser.open("https://www.youtube.com/watch?v=" + item['contentDetails']['videoId'])
+        print()
+        i += 1
+        
+    # need to get nextPageToken and pass in as a pageToken to get next n results
+    
+    # need videoID in https://www.youtube.com/watch?v={videoID}
 
 if __name__ == "__main__":
     main()
